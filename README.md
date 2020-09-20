e8c70d60dfa475a8922a3e9824b91e7fdcfcdd61
donleyg <donleyg@users.noreply.github.com>
2020-09-20 00:52:51 -0400

    index.html sprucing, mongo variables

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Web-Scraping-Challenge
+## Before You Begin
+
+
+## Step 1 - Scraping
+
+Complete your initial scraping using Jupyter Notebook, BeautifulSoup, Pandas, and Requests/Selenium:
+
+* Web Scraping (also termed Screen Scraping, Web Data Extraction, Web Harvesting etc.) is a technique employed to extract large amounts of data from websites whereby the data is extracted and saved to a local file in your computer or to a database in table (spreadsheet) format.
+
+* Use the data you obtained, store it in a Mongo Database, and use those items to append to your own website. In this case, its all about Mars. 
+
+### NASA Mars News 
+[NASA Mars Site](<img src=“static/img/home.png”>)
+
+
+### Mars Facts &&  Featured Image
+[NASA Mars Facts](<img src=“static/img/facts.png”>)
+
+
+### Mars Hemispheres
+[NASA Mars Mosaics](<img src=“static/img/mosaic.png”>)
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Import Dependencies 
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+from flask_bootstrap import Bootstrap
+from flask_assets import Environment, Bundle
+from flask_scss import Scss
 from pymongo import MongoClient
 from Missions_to_Mars import scrape_mars
 import os
@@ -9,6 +12,15 @@ import os
 
 # Create an instance of Flask app
 app = Flask(__name__)
+
+# Enable js features and extend to my templates: 
+assets = Environment(app)
+
+# Route jinjas templating to correct locations:
+Scss(app, static_dir='static', asset_dir='assets')
+
+# Enable bootsrap features and extend to my templates: 
+bootstrap = Bootstrap(app)
 
 
 # Use flask_pymongo to set up mongo connection locally 
@@ -25,7 +37,7 @@ def home():
     mars_dict = mongo.db.mars_dict.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_dict=mars_dict)
+    return render_template("base.html", mars_dict=mars_dict)
 
 # Route that will trigger scrape function
 @app.route("/scrape")
@@ -33,10 +45,11 @@ def scrape():
     # Run scrapped functions
     mars_dict = mongo.db.mars_dict
     scrape_all = scrape_mars.run()
-    # scrape_news = scrape_mars.scrape_news()
-    # scrape_news = scrape_mars.scrape_featured() 
-    # scrape_table = scrape_mars.scrape_table()
-    # scrape_hemisphere = scrape_mars.scrape_hemisphere()
+    # title = mongo.db.mars_dict.find_one_or_404({'title': title})
+    # teaser_html = mongo.db.mars_dict.find_one_or_404({'summary': teaser_html})
+    # featured_image_link = mongo.db.mars_dict.find_one_or_404({'featured_image': featured_image_link})
+    # fact_html = mongo.db.mars_dict.find_one_or_404({'fact_table': fact_html})
+
     mars_dict.update({}, scrape_all, upsert = True)
 
     return redirect("/", code=302)
